0bfaf05b2aa98d4cb0fd609f21e3d43ba19b5e88
arun968670 <124008065+arun968670@users.noreply.github.com>
2023-06-14 11:10:20 +0530

    Msilicon city collegey First Project

---
+++ app.js
@@ -0,0 +1,53 @@
+  // Opening Contact Form
+  function contactMe(){
+    document.getElementById("pop-up").style.display="block"
+}
+
+// User Name Validation
+function userValidation(){
+
+    var ddd = document.getElementById("user").value;
+    namevalidation = /^[a-z]{8}$/g;
+
+    if(namevalidation.test(ddd)){
+        document.getElementById("user-valid").style.display = "block";
+        document.getElementById("user-eoror").style.display = "none";
+    }
+    else{
+        document.getElementById("user-eoror").style.display = "block";
+        document.getElementById("user-valid").style.display = "none";
+
+    }
+}
+// Email Validation
+function emailValidation(){
+
+    var ccc = document.getElementById("email").value;
+    emailvalidation = /^[6-9][0-9]{9}$/
+
+    if(emailvalidation.test(ccc)){
+        document.getElementById("email-valid").style.display = "block";
+        document.getElementById("email-eoror").style.display = "none";
+    }
+    else{
+        document.getElementById("email-eoror").style.display = "block";
+        document.getElementById("email-valid").style.display = "none";
+    }
+}
+// Close The Pop-Up Form
+function clip(){
+    document.getElementById("pop-up").style.display="none"
+}
+// Sending The Data To Storage
+function send(){
+    var ddd = document.getElementById("user").value;
+    namevalidation = /^[a-z]{8}$/
+    var ccc = document.getElementById("email").value;
+    emailvalidation = /^[6-9][0-9]{9}$/
+
+   if(namevalidation.test(ddd) && emailvalidation.test(ccc) ){
+        alert("THE DTAE HAS SEND TO BACK-NED")
+   }
+}
+
+let a = new Promise()
